6141c51c72943f31ce19f6a5843fe6d20be265d5
Marius <mariusganea2202@gmail.com>
2025-12-10 10:38:44 +0000

    Update HeroSection.vue

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -2,17 +2,15 @@
   <div class="pt-24 pb-12">
     <div class="flex items-center justify-center gap-4">
       <img src="/logo.png" alt="logspot logo" class="size-10" />
-      <h1 class="text-4xl font-black">logspot</h1>
+      <h1 class="text-4xl font-black">sequel</h1>
     </div>
     <p class="text-highlighted text-center text-balance mt-12">
-      Logspot is an open source change log template made with Nuxt, Vue, and
-      Tailwindcss. Subscribe to our newsletter to get the latest updates or
-      follow us on
+      Here you will be able to see all the product changelogs made in the past. Join us on
       <a
-        href="https://twitter.com/fayazara"
+        href="https://discord.gg/GsdDKdKScT"
         target="_blank"
         class="text-primary font-medium"
-        >twitter</a
+        >discord</a
       >.
     </p>
   </div>
