9b594e001354124eeaea60154ac5b59434d56f5d
Marius <mariusganea2202@gmail.com>
2025-12-10 11:50:25 +0000

    Update HeroSection.vue

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pt-24 pb-12">
     <div class="flex items-center justify-center gap-4">
-      <img src="/logo.png" alt="logspot logo" class="size-20" />
+      <img src="/logo.png" alt="logspot logo" class="size-40" />
     </div>
     <p class="text-highlighted text-center text-balance mt-12">
       Here you will be able to see all the product changelogs made in the past.
