28817e13da5c350add99a59b92a7a671764a98fa
Marius <mariusganea2202@gmail.com>
2025-12-10 11:46:07 +0000

    Update HeroSection.vue

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="pt-24 pb-12">
     <div class="flex items-center justify-center gap-4">
-      <img src="/logo.png" alt="logspot logo" class="size-10" />
-      <h1 class="text-4xl font-black">sequel</h1>
+      <img src="/logo.png" alt="logspot logo" class="size-20" />
     </div>
     <p class="text-highlighted text-center text-balance mt-12">
-      Here you will be able to see all the product changelogs made in the past. Join us on
-      <a
-        href="https://discord.gg/GsdDKdKScT"
-        target="_blank"
-        class="text-primary font-medium"
-        >discord</a
-      >.
+      Here you will be able to see all the product changelogs made in the past.
     </p>
   </div>
 </template>
